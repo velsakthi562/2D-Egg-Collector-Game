3f7f334915626ccc6c2abe85a492d1ad3a0d3da0
Sakthi <62131636+velsakthi562@users.noreply.github.com>
2020-10-17 17:53:21 +0530

    update

---
+++ Assets/Script/AudioManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public AudioClip eggCraking;
+    public AudioClip collectingEgg;
+    public AudioClip dropEgg;
+
+
+}
