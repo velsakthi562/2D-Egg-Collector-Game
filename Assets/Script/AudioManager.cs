22755d9667b2558c5bffd42bbfc00903ad7775e4
Sakthi <62131636+velsakthi562@users.noreply.github.com>
2020-10-21 15:23:56 +0530

    update

--- Assets/Script/AudioManager.cs
+++ Assets/Script/AudioManager.cs
@@ -4,9 +4,22 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
+    private static AudioManager Instance;
+
     public AudioClip eggCraking;
     public AudioClip collectingEgg;
     public AudioClip dropEgg;
 
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public void PlayAudio(AudioClip clip)
+    {
+
+        //AudioSource.Play(clip);
+
+    }
 
 }
